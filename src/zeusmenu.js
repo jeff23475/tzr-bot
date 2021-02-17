12e3166228c0ec764466d8c66399198769d3694c
jeff23475 <trezuredzn@gmail.com>
2021-02-17 16:47:10 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,12 @@
+const tzrmenu = (prefix, pushname) => {
+    return `❍ *Comandos do Tzr*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.tzrmenu = zeusmenu
